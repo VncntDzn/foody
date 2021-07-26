6c53606b1df6ee9119b20f3955cb0442e27ea9d4
Dizon <vincent.dizon@accenture.com>
2021-07-26 13:45:47 +0800

    added types

--- src/components/TabPanel.tsx
+++ src/components/TabPanel.tsx
@@ -1,26 +1,24 @@
-import { Typography } from '@material-ui/core';
+import { Typography } from "@material-ui/core";
+import { I_Components } from "types/I_Components";
+import PropTypes from "prop-types";
 
-const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
-
+const TabPanel = ({
+  children,
+  value,
+  index,
+  ...other
+}: I_Components.I_TabPanel) => {
   return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} {...other}>
       {value === index && <Typography>{children}</Typography>}
     </div>
   );
 };
-/* 
+
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.any.isRequired,
   value: PropTypes.any.isRequired,
 };
- */
 
 export default TabPanel;
--- src/types/I_Components.ts
+++ src/types/I_Components.ts
@@ -1,6 +1,14 @@
+import { ReactNode } from "react";
 export namespace I_Components {
   export interface I_PaginationTypes {
     pageCount: number;
     onPageChange: () => void;
+  }
+
+  export interface I_TabPanel {
+    children: ReactNode;
+    value: string | number | null;
+    index: number;
+    other: unknown;
   }
 }
