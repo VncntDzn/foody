96d1118977bc28ce6ab6d4668056720f5575c8a9
Vincent Dizon <workvpdizon@gmail.com>
2021-07-26 15:40:57 +0800

    types

--- src/pages/result/Tags.tsx
+++ src/pages/result/Tags.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Typography, Grid } from '@material-ui/core';
+import React from "react";
+import PropTypes from "prop-types";
+import { Typography, Grid } from "@material-ui/core";
 
-import useStyles from './styled';
-const Tags = ({ data }) => {
+import { I_API } from "types/I_API";
+import useStyles from "./styled";
+const Tags = ({ data }: I_API.I_Data) => {
   const styles = useStyles();
   return (
-    <Grid container wrap='wrap' justify='flex-start'>
+    <Grid container wrap="wrap" justify="flex-start">
       <Grid
         container
         item
-        justify='center'
-        alignContent='center'
-        alignItems='center'
+        justify="center"
+        alignContent="center"
+        alignItems="center"
         xs={3}
         md={2}
         sm={2}
         lg={1}
       >
-        <Typography color='primary' align='center'>
+        <Typography color="primary" align="center">
           Tags:
         </Typography>
       </Grid>
 
-      {data?.strTags?.split(',').map((tag, i) => (
-        <Typography key={i} variant='subtitle1'>
+      {data?.strTags?.split(",").map((tag: string, i: number) => (
+        <Typography key={i} variant="subtitle1">
           {tag}, &nbsp;
         </Typography>
       ))}
