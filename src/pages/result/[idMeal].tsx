ef9ad8a9c088cc0178e10b7d524a28bfcf48a1d0
Vincent Dizon <workvpdizon@gmail.com>
2021-07-27 11:03:10 +0800

    styles

--- src/pages/result/[idMeal].tsx
+++ src/pages/result/[idMeal].tsx
@@ -18,7 +18,7 @@ import Instructions from "./Instructions";
 import Ingredients from "./Ingredients";
 import MainLayout from "layouts/MainLayout";
 import axios from "axios";
-import  useStyles from "./sample/useStyles";
+import  useStyles from "./sample/style";
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
   const id = context.params.idMeal;
